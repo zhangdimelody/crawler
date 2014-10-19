d6f6a8396b926223a34ec58dbecfe1045afaa5e4
zhangdi1 <melodyjiangcool@126.com>
2014-10-19 21:38:09 +0800

    ad

--- aol.js
+++ aol.js
@@ -1,63 +1,47 @@
 var express = require('express');
 var crawler = require("crawler");
-var backbone = require("backbone");
 var app = express();
 var crawler = crawler;
-var Backbone = backbone;
 
-app.get('/search', function(req, res){
-	// var now = new Date;
-	return Backbone.View.extend({
-		// template: _.template('<input class="search_input" type="text"/><a href="#" class="search_btn">搜索</a>')
-		events:{
-			"click .search_btn"   : "searchResult"
-		}
-		,initialize:function(){
+app.get('/',function(req,res){
+	res.sendfile('aol.html');
+});
 
-		}
-		,render:function(){
-			alert(0)
-			this.$el.html('<input class="search_input" type="text"/><a href="#" class="search_btn">搜索</a>');
-			return this;
-		}
-		,searchResult:function(){
-			var Crawler = crawler.Crawler;
-			var keyword="grunt";
-			var url = 'http://search.aol.com/aol/search?v_t=comsearch&enabled_terms=&q='+keyword+'&page=';
-			var urls = [];
-			var max = 1;
-			for(var i=1;i<=max;i++){
-				urls.push(url+i);
-			}
+// 匹配所有以 /search 开头的请求
+app.get(/^\/search/, function(req, res){
+	var Crawler = crawler.Crawler;
+	
+	var keyword="grunt";
+	var url = 'http://search.aol.com/aol/search?v_t=comsearch&enabled_terms=&q='+keyword+'&page=';
+	var urls = [];
+	var max = 1;
+	for(var i=1;i<=max;i++){
+		urls.push(url+i);
+	}
 
-			var results = [];
+	var results = [];
 
-			var cnt = 1;
-			var c = new Crawler({
-				"maxConnections":10,
-				"callback":function(error,result,$) {
-					$('.hac a').each(function(){ 
-						results.push ( $(this).attr('href') ); 
-					});
-
-					if(cnt++ == max){
-						// (new Date() - now)
-						res.send("<h1>" +"</h1>" + results.join(""));
-
-					}
-
-					
-				}
+	var cnt = 1;
+	var c = new Crawler({
+		"maxConnections":10,
+		"callback":function(error,result,$) {
+			$('.hac a').each(function(){ 
+				results.push ( $(this).attr('href') ); 
 			});
 
+			if(cnt++ == max){
+				// (new Date() - now)
+				res.send("<h1>" +"</h1>" + results.join(""));
 
-			c.queue(urls);
+			}
+
+			
 		}
 	});
 
-	// view.render();
 
-	
+	c.queue(urls);
+
   
 });
 
---
+++ test.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var app = express();
+app.use(function(req, res, next) {
+  res.send('Hello World');
+})
